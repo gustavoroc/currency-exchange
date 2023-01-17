60154c75b8024a2922bc9edb51dcf93311647006
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2023-01-17 16:07:11 -0300

    test: ExchangeRateCurrencyService class tested for the methods getAllSymbols and convertExchange with success

---
+++ src/app/infra/exchange-rate/exchange-rate-currency.spec.ts
@@ -0,0 +1,59 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { ExchangeRateCurrencyService } from './exchange-rate-currency.service';
+import { environment } from 'src/environments/environment';
+
+import mockedAllSymbols from './mocks/all-symbols.mock.json';
+import mockedCurrencyExchange from './mocks/currency-exchange.mock.json';
+
+import allSymbolsResponse from './mocks/fixtures/all-symbols-response.fixture.json';
+import currencyExchangeResponse from './mocks/fixtures/currency-exchange-response.fixture.json';
+
+describe(ExchangeRateCurrencyService.name, () => {
+  let serviceRef: ExchangeRateCurrencyService;
+  let mockedHttpClient: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ExchangeRateCurrencyService],
+    }).compileComponents();
+
+    serviceRef = TestBed.inject(ExchangeRateCurrencyService);
+    mockedHttpClient = TestBed.inject(HttpTestingController);
+  });
+
+  it(`${ExchangeRateCurrencyService.prototype.getAllSymbols.name} should return all the symbols supported by the API`, (done) => {
+    serviceRef.getAllSymbols().subscribe((data) => {
+      expect(data).toEqual(mockedAllSymbols);
+      done();
+    });
+
+    mockedHttpClient
+      .expectOne(`${environment.API_URL}/symbols`)
+      .flush(allSymbolsResponse);
+  });
+
+  it(`${ExchangeRateCurrencyService.prototype.convertExchange.name}`, (done) => {
+    const currenciesToBeConverted = {
+      from: 'EUR',
+      to: 'USD',
+      amount: 50,
+    };
+
+    serviceRef.convertExchange('EUR', 'USD', '50').subscribe((data) => {
+      expect(data).toEqual(mockedCurrencyExchange);
+      done();
+    });
+
+    mockedHttpClient
+      .expectOne(
+        `${environment.API_URL}/convert?from=${currenciesToBeConverted.from}&to=${currenciesToBeConverted.to}&amount=${currenciesToBeConverted.amount}`
+      )
+      .flush(currencyExchangeResponse);
+  });
+});
