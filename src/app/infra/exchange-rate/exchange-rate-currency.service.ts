4c6df8b4606eec08b0ece7cd9d7dd6bfa1aa5fea
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2023-01-17 16:18:07 -0300

    feat: ExchangeRate api service implements CurrencyApi contract

--- src/app/infra/exchange-rate/exchange-rate-currency.service.ts
+++ src/app/infra/exchange-rate/exchange-rate-currency.service.ts
@@ -1,33 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { CurrencyApiService } from 'src/app/usecases/currency-api.service.usecase';
 import { environment } from 'src/environments/environment';
 
-export interface SymbolContent {
-  [SymbolName: string]: {
-    description: string;
-    code: string;
-  };
-}
-
-export interface CurrencyExchangeMock {
-  query: {
-    from: string;
-    to: string;
-    amount: number;
-  };
-  info: {
-    rate: number;
-  };
-  date: string;
-  result: number;
-}
-
 @Injectable()
-export class ExchangeRateCurrencyService {
+export class ExchangeRateCurrencyService implements CurrencyApiService {
   constructor(private httpClient: HttpClient) {}
 
-  getAllSymbols(): Observable<SymbolContent> {
+  getAllSymbols(): Observable<any> {
     return this.httpClient
       .get<any>(`${environment.API_URL}/symbols`)
       .pipe(map((data) => data.symbols));
@@ -37,7 +18,7 @@ export class ExchangeRateCurrencyService {
     from: string,
     to: string,
     amount: string = '1'
-  ): Observable<CurrencyExchangeMock> {
+  ): Observable<any> {
     return this.httpClient
       .get<any>(
         `${environment.API_URL}/convert?from=${from}&to=${to}&amount=${amount}`
