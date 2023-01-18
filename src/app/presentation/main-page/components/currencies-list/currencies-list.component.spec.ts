6cf26db56dbaeed981d981d27f4038ed5c1fc376
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2023-01-18 18:53:01 -0300

    test: Implementation of pagination in the currenciesListComponent

--- src/app/presentation/main-page/components/currencies-list/currencies-list.component.spec.ts
+++ src/app/presentation/main-page/components/currencies-list/currencies-list.component.spec.ts
@@ -1,9 +1,12 @@
+import { of } from 'rxjs';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { CurrencyAdapterService } from 'src/app/adapters/currency-adapter.service';
 import { CurrencyApiStub } from 'src/app/adapters/test/currency-api.stub';
 import { CurrencyAdapterInterfaceToken } from 'src/app/injection-tokens/currency-adapter-service.di.token';
 import { CurrencyApiInterfaceToken } from 'src/app/injection-tokens/currency-api-service.di.token';
 import { CurrenciesListComponent } from './currencies-list.component';
+import { Currency } from 'src/app/models/currency.model';
 
 describe(CurrenciesListComponent.name, () => {
   let fixture: ComponentFixture<CurrenciesListComponent>;
@@ -30,9 +33,45 @@ describe(CurrenciesListComponent.name, () => {
   });
 
   it(`${CurrenciesListComponent.prototype.currenciesList} should have the list of currencies, when the component inits`, (done) => {
-    component.currenciesList?.subscribe((currenciesList) => {
-      expect(currenciesList.length > 0).toBeTrue();
-      done();
-    });
+    expect(component.currenciesList.length > 0).toBeTrue();
+    done();
+  });
+
+  it(`${CurrenciesListComponent.prototype.paginatedList} should return the paginated list of currencies`, () => {
+    const listOfCurrencies: Currency[] = [
+      {
+        fullName: 'fake_1',
+        acronym: 'fake_1',
+      },
+      {
+        fullName: 'fake_2',
+        acronym: 'fake_2',
+      },
+      {
+        fullName: 'fake_3',
+        acronym: 'fake_3',
+      },
+      {
+        fullName: 'fake_4',
+        acronym: 'fake_4',
+      },
+      {
+        fullName: 'fake_5',
+        acronym: 'fake_5',
+      },
+      {
+        fullName: 'fake_6',
+        acronym: 'fake_6',
+      },
+    ];
+
+    component.currenciesList = listOfCurrencies;
+
+    component.paginatedList();
+    expect(component.currenciesListPaginated.length).toBe(5);
+
+    component.currenciesCurrentPage = 1;
+    component.paginatedList();
+    expect(component.currenciesListPaginated.length).toBe(1);
   });
 });
