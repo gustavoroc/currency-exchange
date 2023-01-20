6a25dcc6431a927cb44e50cb4980a61d7cb1df5d
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2023-01-20 20:48:37 -0300

    feat: Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './presentation/main-page/main-page.component';
+import { MainPageModule } from './presentation/main-page/main-page.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: MainPageComponent,
+    path: '',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), MainPageModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
