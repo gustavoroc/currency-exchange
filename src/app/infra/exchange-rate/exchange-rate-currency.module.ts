a39d8550b9b477e608eb67a788629ebada29113b
Gustavo R. Pereira <gustavo.rocha@srssistemas.com>
2023-01-20 20:49:31 -0300

    fix: exchangeRateCurrency module aren	 using injection token

--- src/app/infra/exchange-rate/exchange-rate-currency.module.ts
+++ src/app/infra/exchange-rate/exchange-rate-currency.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ExchangeRateCurrencyService } from './exchange-rate-currency.service';
+import { CurrencyApiInterfaceToken } from 'src/app/injection-tokens/currency-api-service.di.token';
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [
     {
-      provide: 'CurrencyApiService',
+      provide: CurrencyApiInterfaceToken,
       useClass: ExchangeRateCurrencyService,
     },
   ],
