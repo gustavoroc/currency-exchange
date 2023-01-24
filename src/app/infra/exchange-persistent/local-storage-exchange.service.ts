72afd68c2a4ed1d1528ec8cd8114c748ff6b19bd
Gustavo R. Pereira <gustavdrp@gmail.com>
2023-01-24 19:13:52 -0300

    feat: LocalStorageExchangeService implemented

---
+++ src/app/infra/exchange-persistent/local-storage-exchange.service.ts
@@ -0,0 +1,39 @@
+import { CurrencyExchange } from 'src/app/models/currency.model';
+import { CurrencyExchangePersistentService } from 'src/app/usecases/currency-persistent.service.usecase';
+
+export class LocalStorageExchangeService
+  implements CurrencyExchangePersistentService
+{
+  private static readonly CURRENCY_EXCHANGE_KEY = 'currency_exchange';
+
+  addASingleExchange(exchange: CurrencyExchange): void {
+    const exchanges = this.getAll();
+    localStorage.setItem(
+      LocalStorageExchangeService.CURRENCY_EXCHANGE_KEY,
+      JSON.stringify(exchanges.push(exchange))
+    );
+  }
+
+  getAll(): CurrencyExchange[] {
+    const exchangeString = localStorage.getItem(
+      LocalStorageExchangeService.CURRENCY_EXCHANGE_KEY
+    );
+    return exchangeString ? JSON.parse(exchangeString) : [];
+  }
+
+  add(exchange: CurrencyExchange[]): void {
+    localStorage.setItem(
+      LocalStorageExchangeService.CURRENCY_EXCHANGE_KEY,
+      JSON.stringify(exchange)
+    );
+  }
+
+  delete(id: string): void {
+    const exchange = this.getAll();
+    const updatedExchange = exchange.filter((ex) => ex.id !== id);
+    localStorage.setItem(
+      LocalStorageExchangeService.CURRENCY_EXCHANGE_KEY,
+      JSON.stringify(updatedExchange)
+    );
+  }
+}
