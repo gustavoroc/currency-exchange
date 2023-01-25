5a9096b642ae31b4479fa6528e157691e98b2264
Gustavo R. Pereira <gustavdrp@gmail.com>
2023-01-25 14:23:04 -0300

    feat: MainPageModule supports localStorageExchangeModule

--- src/app/presentation/main-page/main-page.module.ts
+++ src/app/presentation/main-page/main-page.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CurrencyAdapterModule } from 'src/app/adapters/currency-adapter.module';
+import { LocalStorageExchangeModule } from 'src/app/infra/exchange-persistent/local-storage-exchange.module';
 import { CurrenciesModule } from './components/currencies/currencies.module';
 import { MakeExchangeComponent } from './components/make-exchange/make-exchange.component';
 import { SelectExchangeComponent } from './components/select-exchange/select-exchange.component';
@@ -13,6 +14,11 @@ import { MainPageComponent } from './main-page.component';
     MakeExchangeComponent,
   ],
   exports: [MainPageComponent],
-  imports: [CurrenciesModule, CurrencyAdapterModule, CommonModule],
+  imports: [
+    CurrenciesModule,
+    CurrencyAdapterModule,
+    CommonModule,
+    LocalStorageExchangeModule,
+  ],
 })
 export class MainPageModule {}
