40cb4ec182f076094702a45db3dd6a995de5038e
Gustavo R. Pereira <gustavdrp@gmail.com>
2023-01-25 17:00:17 -0300

    feat: HistoryPageComponent implemented with angular material

--- src/app/presentation/history-page/history-page.component.ts
+++ src/app/presentation/history-page/history-page.component.ts
@@ -1,4 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import {
+  MatTable,
+  MatTableDataSource,
+  _MatTableDataSource,
+} from '@angular/material/table';
 import { CurrencyPersistentInterfaceServiceToken } from 'src/app/injection-tokens/currency-persistent.service.di.token';
 import { CurrencyExchange } from 'src/app/models/currency.model';
 import { CurrencyExchangePersistentService } from 'src/app/usecases/currency-persistent.service.usecase';
@@ -8,12 +14,30 @@ import { CurrencyExchangePersistentService } from 'src/app/usecases/currency-per
   selector: 'app-history-page',
 })
 export class HistoryPageComponent implements OnInit {
+  persistedExchanges?: CurrencyExchange[];
+  dataSource: MatTableDataSource<CurrencyExchange>;
+  displayedColumns = [
+    'currencyFrom',
+    'valueFrom',
+    'currencyTo',
+    'valueTo',
+    'rate',
+    'date',
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource(this.persistedExchanges);
+    this.dataSource.paginator = this.paginator;
+  }
+
   constructor(
     @Inject(CurrencyPersistentInterfaceServiceToken)
     private currencyPersistentService: CurrencyExchangePersistentService
-  ) {}
-
-  persistedExchanges?: CurrencyExchange[];
+  ) {
+    this.dataSource = new MatTableDataSource(this.persistedExchanges);
+  }
 
   ngOnInit(): void {
     this.persistedExchanges = this.currencyPersistentService.getAll();
--- src/app/presentation/history-page/history-page.module.ts
+++ src/app/presentation/history-page/history-page.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HistoryPageComponent } from './history-page.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [HistoryPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule],
 })
 export class HistoryPageModule {}
