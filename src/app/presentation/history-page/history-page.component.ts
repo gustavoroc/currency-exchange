b94b2d212db17197dbb06f3294d020cacf5e2116
Gustavo R. Pereira <gustavdrp@gmail.com>
2023-01-25 15:40:06 -0300

    feat: scaffold page for history of exchanges

--- src/app/presentation/history-page/history-page.component.ts
+++ src/app/presentation/history-page/history-page.component.ts
@@ -1,7 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { CurrencyPersistentInterfaceServiceToken } from 'src/app/injection-tokens/currency-persistent.service.di.token';
+import { CurrencyExchange } from 'src/app/models/currency.model';
+import { CurrencyExchangePersistentService } from 'src/app/usecases/currency-persistent.service.usecase';
 
 @Component({
   templateUrl: './history-page.component.html',
   selector: 'app-history-page',
 })
-export class HistoryPageComponent {}
+export class HistoryPageComponent implements OnInit {
+  constructor(
+    @Inject(CurrencyPersistentInterfaceServiceToken)
+    private currencyPersistentService: CurrencyExchangePersistentService
+  ) {}
+
+  persistedExchanges?: CurrencyExchange[];
+
+  ngOnInit(): void {
+    this.persistedExchanges = this.currencyPersistentService.getAll();
+  }
+}
